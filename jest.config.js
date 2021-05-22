29d1a8603d8ddc6a0420ecd10c7a539a18946e97
Raphael Gibson <raphael.araujo@estantemagica.com.br>
2021-05-22 19:58:02 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  roots: [
+    '<rootDir>/src'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
