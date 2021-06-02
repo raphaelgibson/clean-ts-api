a29f3e1d11d95572701c7832fbfca34bd67decad
Raphael Gibson <raphael.araujo@estantemagica.com.br>
2021-06-02 14:58:30 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -2,12 +2,17 @@ import { AccountMongoRepository } from './account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 describe('Account Mongo Repository', () => {
-  beforeAll(async () => {
+  beforeAll(async (): Promise<void> => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
 
-  afterAll(async () => {
+  afterAll(async (): Promise<void> => {
     await MongoHelper.disconnect()
+  })
+
+  beforeEach(async (): Promise<void> => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   const makeSut = (): AccountMongoRepository => {
