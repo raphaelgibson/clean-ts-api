478e28e9c3c5c3052690690522e914d3c2480068
Raphael Gibson <raphael.araujo@estantemagica.com.br>
2022-04-16 15:09:04 -0300

    test: ensure RequiredFieldValidation returns a MissingParamError if validation fails

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,6 +1,6 @@
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
-import { RequireFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { EmailValidator } from '../../presentation/protocols/email-validator'
@@ -23,7 +23,7 @@ describe('SignUpValidation Factory', () => {
     makeSignUpValidation()
     const validations: Validation[] = []
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-      validations.push(new RequireFieldValidation(field))
+      validations.push(new RequiredFieldValidation(field))
     }
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     validations.push(new EmailValidation('email', makeEmailValidator()))
