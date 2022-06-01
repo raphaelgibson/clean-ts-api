c21d3cc4613f008cc798ff8c823eafdfbb3e18eb
Raphael Gibson <raphael.araujo@estantemagica.com.br>
2022-06-01 01:30:56 -0300

    test: ensure SurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,39 @@
+import { SurveyMongoRepository } from './survey-mongo-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+
+describe('Survey Mongo Repository', () => {
+  beforeAll(async (): Promise<void> => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async (): Promise<void> => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async (): Promise<void> => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should add a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answer: 'any_answer'
+      }, {
+        answer: 'other_answer'
+      }]
+    })
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
