a743402b0a923ab21f202fba20fd8144ce073975
Raphael Gibson <raphael.araujo@estantemagica.com.br>
2022-06-27 22:01:48 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,13 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveys } from '../../../domain/usecases/load-surveys'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (private readonly addSurveyRepository: LoadSurveysRepository) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.addSurveyRepository.loadAll()
+
+    return []
+  }
+}
