f5dc698a154058b69a68fe4df243deaf4d39062b
Raphael Gibson <raphael.araujo@estantemagica.com.br>
2022-07-30 18:39:48 -0300

    refactor: use CheckAccountByEmailRepository instead LoadAccountByEmailRepository in DbAddAccount

--- tests/data/mocks/mock-db-account.ts
+++ tests/data/mocks/mock-db-account.ts
@@ -1,4 +1,4 @@
-import { AddAccountRepository, LoadAccountByEmailRepository, LoadAccountByTokenRepository, UpdateAccessTokenRepository } from '@/data/protocols'
+import { AddAccountRepository, CheckAccountByEmailRepository, LoadAccountByEmailRepository, LoadAccountByTokenRepository, UpdateAccessTokenRepository } from '@/data/protocols'
 
 import faker from 'faker'
 
@@ -8,6 +8,17 @@ export class AddAccountRepositorySpy implements AddAccountRepository {
 
   async add (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     this.addAccountParams = data
+
+    return await Promise.resolve(this.result)
+  }
+}
+
+export class CheckAccountByEmailRepositorySpy implements CheckAccountByEmailRepository {
+  result = false
+  email: string
+
+  async checkByEmail (email: string): Promise<CheckAccountByEmailRepository.Result> {
+    this.email = email
 
     return await Promise.resolve(this.result)
   }
